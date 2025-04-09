cf9b9324a9d82bcbb264c058e2d55e70e88f1ef5
AnjaliTurumella <155085884+AnjaliTurumella@users.noreply.github.com>
2025-04-09 20:02:33 +0530

    Update db.py

--- LAF_flask/db.py
+++ LAF_flask/db.py
@@ -4,7 +4,7 @@ import datetime
 
 def get_db():
     # MongoDB connection (local or Atlas)
-    mongo_uri = os.getenv('MONGO_URI', 'mongodb://localhost:27017/')
+    mongo_uri = os.getenv('MONGO_URI', 'mongodb+srv://lostandfound:WDJJ5t.ZchRQAxj@cluster0.qqahq6s.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0')
     client = MongoClient(mongo_uri)
     db = client['LAF_db']
     return db
